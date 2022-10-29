cf25bc771d2feda14d4a7af793c77da8e2a814f4
yahnc <sebastianhpancho@gmail.com>
2022-10-29 16:46:58 -0500

    Update SingletonMain.java

--- src/main/java/singleton/SingletonMain.java
+++ src/main/java/singleton/SingletonMain.java
@@ -3,13 +3,14 @@ package singleton;
 public class SingletonMain {
 
     public static void main(String[]arg){
-        MongoDBClient mongoDBClient = MongoDBClient.getClient();
-        System.out.println(mongoDBClient.getData());
-        MongoDBClient mongoDBClient2 = MongoDBClient.getClient();
-        System.out.println(mongoDBClient2.getData());
-        MongoDBClient mongoDBClient3 = MongoDBClient.getClient();
-        System.out.println(mongoDBClient3.getData());
-
+//        MongoDBClient mongoDBClient = MongoDBClient.getClient();
+//        System.out.println(mongoDBClient.getData());
+//        MongoDBClient mongoDBClient2 = MongoDBClient.getClient();
+//        System.out.println(mongoDBClient2.getData());
+//        MongoDBClient mongoDBClient3 = MongoDBClient.getClient();
+//        System.out.println(mongoDBClient3.getData());
+dbConexion.getConnection();
+System.out.println(OK);
 
     }
 }
